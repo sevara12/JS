5fc2ee05629ad13815ee7762f21b8ff0e476b308
Sevara <sevarahamdamova499@gmail.com>
2021-11-23 11:25:53 -0800

    JS

---
+++ script.js
@@ -0,0 +1,56 @@
+// name
+
+var name = prompt('Enter your name')
+
+alert (name)
+
+console.log(name);
+
+
+// age
+var age = prompt('Enter your age');
+
+alert (age)
+
+console.log(age);
+
+// tasks
+var plus = +prompt('5 + 4 = ?')
+
+alert (plus)
+
+console.log( ' 5 + 4 = 9 ' + ' Your answer is ' + plus);
+
+
+
+var minus = +prompt('5 - 4 = ?')
+
+alert(minus)
+
+console.log(' 5 - 4 = 1 ' + ' Your answer is ' + minus);
+
+
+
+var multiply = +prompt('5 * 4 = ?')
+
+alert (multiply)
+
+console.log(' 5 * 4 = 20 ' + ' Your answer is ' + multiply);
+
+
+
+var devison = +prompt('5 / 5 = ?')
+
+alert (division)
+
+console.log(' 5 / 5 = 2 ' + ' Your answer is ' + division);
+
+
+
+var percent = +prompt('5 % 4 = ?')
+
+alert (percent)
+
+console.log(' 5 % 4 = 1 ' + ' Your answer is ' + percent);
+
+alert('Go to console to see your answers')
